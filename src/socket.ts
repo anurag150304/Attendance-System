069b8101efdf1bc7d2b37d3b21648bc4e65b60de
Anurag Mishra <anurag.mishra150304@gmail.com>
2026-01-22 17:20:02 +0000

    Updated

--- src/socket.ts
+++ src/socket.ts
@@ -2,9 +2,11 @@ import { WebSocketServer, WebSocket, type Server as SocketServer } from "ws";
 import type { IWebSocket, JWTPayload } from "./types/common.type.js";
 import { verifyToken } from "./utils/common.util.js";
 import type { Server } from "http";
+import { activeSession } from "./app.js";
 
 let io: SocketServer | null = null;
 const clients: WebSocket[] = [];
+const ID = () => Math.floor(Math.random() * 10_000);
 
 export async function initializeSocketServer(server: Server) {
     io = new WebSocketServer({ server, port: 3000 });
@@ -15,42 +17,47 @@ export async function initializeSocketServer(server: Server) {
         if (!req?.url) throw new Error("Url not found");
         const token = req.url.split("?")[1]?.trim().split("=")[1]?.trim();
         if (!token) {
-            webSocket.emit("ERROR", { data: { message: "Unauthorized or invalid token" } });
-            return io?.close();
+            webSocket.send(JSON.stringify({ event: "ERROR", data: { message: "Unauthorized or invalid token" } }));
+            return;
         }
 
         try {
             const decodedData: JWTPayload = verifyToken(token!);
             if (!decodedData) {
-                webSocket.emit("ERROR", { data: { message: "Invalid token payload" } });
-                return io?.close();
+                webSocket.send(JSON.stringify({ event: "ERROR", data: { message: "Invalid token payload" } }));
+                return;
             };
             webSocket.user = { userId: decodedData.userId, role: decodedData.role }
         } catch (err) {
-            webSocket.emit("ERROR", { data: { message: (err as Error).message || "Failed to verify token!" } });
-            return io?.close();
+            webSocket.send(JSON.stringify({ event: "ERROR", data: { message: (err as Error).message || "Failed to verify token!" } }));
+            return;
         }
 
-        // webSocket.on("")
+        console.log(`Socket ID connected ${ID()}`);
 
-        webSocket.on("ATTENDANCE_MARKED", (payload) => {
-            const data = JSON.parse(payload.toLocalString());
-        });
+        webSocket.on("message", (payload: WebSocket.RawData) => {
+            const { event, data } = JSON.parse(payload.toString());
+            if (!event) throw new Error("Event is required!");
+            if (!data) throw new Error("Payload data is required!");
 
-        webSocket.on("ERROR", (payload) => {
-            const data = JSON.parse(payload.toLocalString());
+            if (event === "ERROR") {
+
+            }
+
+            if (event === "ATTENDANCE_MARKED") {
+                if (webSocket.user.role !== "teacher") webSocket.send(JSON.stringify({
+                    event: "ERROR",
+                    data: { message: "Forbidden, teacher event only" }
+                }));
+
+                if (!activeSession) webSocket.send(JSON.stringify({
+                    event: "ERROR",
+                    data: { "message": "No active attendance session" }
+
+                }));
+
+
+            }
         });
     });
-}
-
-export function sendMessage(event: string, payload: any) {
-    if (!event) throw new Error("Event name is required!");
-    if (!payload) throw new Error("Payload is required!");
-    if (!io) throw new Error("Socket connection is not established!");
-
-    try {
-        io.emit(event,)
-    } catch (err) {
-
-    }
 }
